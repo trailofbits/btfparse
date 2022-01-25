f00362eda19c71cf3e4f01fa7c7011109d928abc
Alessandro Gario <alessandro.gario@gmail.com>
2022-01-25 18:48:20 +0100

    tools: Add a bpftool compatibility test for dump-btf

--- btfparse/filereader/tests/main.cpp
+++ btfparse/filereader/tests/main.cpp
@@ -1,3 +1,11 @@
+//
+// Copyright (c) 2021-present, Trail of Bits, Inc.
+// All rights reserved.
+//
+// This source code is licensed in accordance with the terms specified in
+// the LICENSE file found in the root directory of this source tree.
+//
+
 #include "filereader.h"
 
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
