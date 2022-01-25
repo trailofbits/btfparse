925671c52521b500e5ebbbaaf0860a1712c69ff7
Alessandro Gario <alessandro.gario@gmail.com>
2022-01-25 18:22:23 +0100

    utils: Add tests for the Result class

--- btfparse/utils/tests/main.cpp
+++ btfparse/utils/tests/main.cpp
@@ -1,13 +1,2 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
-
-#include <btfparse/error.h>
-#include <btfparse/result.h>
-
-namespace btfparse {
-
-TEST_CASE("Result class") { CHECK(true); }
-
-TEST_CASE("Error class") { CHECK(true); }
-
-} // namespace btfparse
