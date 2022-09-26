eb06dd2addc3eb5ddc8d324e6189b6c20ffe71c2
Alessandro Gario <alessandro.gario@gmail.com>
2022-09-27 01:49:38 +0200

    cmake: Do not enable tests by default

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -21,7 +21,7 @@ if(NOT "${CMAKE_BUILD_TYPE}" STREQUAL "Release" AND
 endif()
 
 option(BTFPARSE_ENABLE_TOOLS "Set to ON to build the tools" false)
-option(BTFPARSE_ENABLE_TESTS "Set to ON to build the tests" true)
+option(BTFPARSE_ENABLE_TESTS "Set to ON to build the tests" false)
 option(BTFPARSE_OMIT_FRAME_POINTERS "Set to ON to omit frame pointers" true)
 
 set(CMAKE_EXPORT_COMPILE_COMMANDS true CACHE BOOL "Export the compile_commands.json file (forced)" FORCE)
